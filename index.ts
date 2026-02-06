0b848f316af54bd9dec1f17796d353b33c06609a
ranjit <ranjitd-sys>
2026-02-06 17:24:23 +0530

    adding to main

--- index.ts
+++ index.ts
@@ -1,16 +1,17 @@
-import { readFile, utils } from 'xlsx';
+import { readFile, utils, type WorkSheet } from 'xlsx';
+import type { TransactionRecord } from './types';
 
 // 1. Load the workbook
 const workbook = readFile('data.xlsx');
 
 // 2. Get the name of the first sheet
-const sheetName = workbook.SheetNames[0];
+const sheetName  = workbook.SheetNames[0] ;
 
-// 3. Get the content of that sheet
+// @ts-ignore
 const worksheet = workbook.Sheets[sheetName] ;
 
 // 4. Convert the sheet data to JSON and print it
-const data = utils.sheet_to_json(worksheet);
+const data: TransactionRecord[] = utils.sheet_to_json(worksheet);
 
 console.log("--- Spreadsheet Data ---");
-data.map(data =>  console.log(data))
+data.map(data =>  console.log(data['Closing Fee']))
