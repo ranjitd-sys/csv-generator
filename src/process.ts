3add06f61bf12fe41c5525f525723d9489d99a78
ranjitd-sys <ranjitd@deepecom.com>
2026-02-06 23:07:50 +0530

    adding this thigs

--- src/process.ts
+++ src/process.ts
@@ -1,17 +1,17 @@
 import { readFile, utils, type WorkSheet } from 'xlsx';
 import type { TransactionRecord } from './types';
-import { Effect } from 'effect';
-import { NotFound } from './ error';
+import { Data, Effect } from 'effect';
+import { NotFound } from './error';
 import { daemonChildren } from 'effect/Effect';
 
 
 export const convertData = Effect.gen(function* () {
-     const data = yield* Effect.try({
+    const data = yield* Effect.try({
         try: () => {
             const workbook = readFile('../public/data.xlsx');
             const sheetName = workbook.SheetNames[4];
             const worksheet = workbook.Sheets[sheetName || ""];
-            if(!worksheet) throw new NotFound();
+            if (!worksheet) throw new NotFound();
             return utils.sheet_to_json<TransactionRecord>(worksheet)
         },
         catch: (e) => e instanceof NotFound
@@ -19,9 +19,20 @@ export const convertData = Effect.gen(function* () {
     return data;
 })
 
-export const main = Effect.gen(function * (){
+export const main = Effect.gen(function* () {
     const jsonCsv = yield* convertData;
-    jsonCsv.forEach(data => console.log(data))
+    const filterDate = jsonCsv.filter(res => {
+        if (res['Closing Fee'] !== 0 || 
+            res['Courier Fee'] !== 0 || 
+            res['Compensation Cess'] !== 0 ||
+             res['Marketing Fee'] !== 0 ||
+             res['Fulfillment Fee']||
+
+              res['Payment Collection Fee']) {
+                return res;
+        }
+    })
+    console.log(filterDate)
 })
 
 const res = await Effect.runPromise(main);
